eb13200146a3c0ddecf946eefbcafc9a29dcf9b9
JeffreyStocker <kagesennin@gmail.com>
2018-02-03 22:25:20 -0800

    Refac: updated public html to use classes instead of ids, also added more data to pump info, and renamed the pump state to better demonstrate that is if the pump is running rather than pump power

--- public/index.js
+++ public/index.js
@@ -30,55 +30,55 @@
 // }
 var socket, messageTimer;
 var showMessage = function () {
-  $('#message').text('Message Sent');
+  $('.message').text('Message Sent');
   messageTimer = setTimeout(() => {
-    $('#message').html('&nbsp;');
+    $('.message').html('&nbsp;');
   }, 2000);
 };
 
 var confirmMessage = function () {
-  $('#message').text('Message Confirmed');
+  $('.message').text('Message Confirmed');
   // alert('received')
   messageTimer = setTimeout(() => {
-    $('#message').html('&nbsp;');
+    $('.message').html('&nbsp;');
   }, 2000);
 };
 
 
 $('document').ready(function () {
   socket = io.connect();
-  $('#powerOn').click(function () {
+  $('.powerOn').click(function () {
     socket.emit('pumpPower', 'on', confirmMessage);
     showMessage();
   });
 
-  $('#powerOff').click(function () {
+  $('.powerOff').click(function () {
     socket.emit('pumpPower', 'off', () => {
       showMessage();
     });
   });
 
-  $('#powerToggle').click(function () {
+  $('.powerToggle').click(function () {
     socket.emit('pumpPower', 'toggle', confirmMessage);
     showMessage();
   });
 
-  $('#speed1').click(function () {
+  $('.speed1').click(function () {
     socket.emit('intellicom', 1, confirmMessage);
     showMessage();
   });
 
-  $('#speed2').click(function () {
+  $('.speed2').click(function () {
     socket.emit('intellicom', 2, confirmMessage);
     showMessage();
   });
 
-  $('#speed3').click(function () {
+  $('.speed3').click(function () {
     socket.emit('intellicom', 3, confirmMessage);
     showMessage();
   });
 
-  $('#speed4').click(function () {
+  $('.speed4').click(function () {
     socket.emit('intellicom', 4, confirmMessage);
     showMessage();
   });
@@ -88,48 +88,48 @@ $('document').ready(function () {
     showMessage();
   });
 
-  $('#test').click(function () {
+  $('.test').click(function () {
     socket.emit('test_runpumpSpeedAt1000RPM', confirmMessage);
     showMessage();
   });
 
-  $('#toggleStatusUpdates').click(function () {
+  $('.toggleStatusUpdates').click(function () {
     socket.emit('toggleStatusUpdate', confirmMessage);
     showMessage();
   });
 
-  $('#pumpStatus').click(function () {
+  $('.pumpStatus').click(function () {
     socket.emit('pumpData', confirmMessage);
     showMessage();
   });
 
-  $('#pumpStatusForce').click(function () {
+  $('.pumpStatusForce').click(function () {
     socket.emit('pumpDataForceUpdate', confirmMessage);
     showMessage();
   });
 
 
-  $('#runPumpAtSpeed').click(function () {
-    socket.emit('runPumpAtSpeed', $('#speedInput').val(), confirmMessage );
+  $('.runPumpAtSpeed').click(function () {
+    socket.emit('runPumpAtSpeed', $('.speedInput').val(), confirmMessage );
     showMessage();
   });
 
-  $('#setPumpTimer').click(function () {
-    socket.emit('setPumpTimer', $('#timerInMin').val(), confirmMessage );
+  $('.setPumpTimer').click(function () {
+    socket.emit('setPumpTimer', $('.timerInMin').val(), confirmMessage );
     showMessage();
   });
 
-  $('#pumpControlSetToRemote').click(function () {
+  $('.pumpControlSetToRemote').click(function () {
     socket.emit('pumpControlPanelState', 'remote', confirmMessage);
     showMessage();
   });
 
-  $('#pumpControlSetToLocal').click(function () {
+  $('.pumpControlSetToLocal').click(function () {
     socket.emit('pumpControlPanelState', 'local', confirmMessage);
     showMessage();
   });
 
-  $('#pumpControlSetToggle').click(function () {
+  $('.pumpControlSetToggle').click(function () {
     socket.emit('pumpControlPanelState', 'toggle', confirmMessage);
     showMessage();
   });
@@ -146,40 +146,58 @@ $('document').ready(function () {
   socket.on('pumpDataReturn', function (data) {
     // console.log(data);
     // alert(JSON.stringify(data))
-    data.state === 4 ? $('#pumpState').text('Off') : $('#pumpState').text('On');
-    // $('#pumpState').text(data.state)
-    data.timer === '0:0' ? $('#PumpTimers').text('Off') : $('#PumpTimers').text(data.timer);
-    // $('#PumpTimers').text(data.timer)
-    $('#pumpRPM').text(data.rpm);
-    $('#pumpWatt').text(data.watt);
+    data.state === 4 ? $('.pumpState').text('No') : $('.pumpState').text('Yes');
+    // $('.pumpState').text(data.state)
+    data.timer === '0:0' ? $('.PumpTimers').text('Off') : $('.PumpTimers').text(data.timer);
+    // $('.PumpTimers').text(data.timer)
+    $('.pumpRPM').text(data.rpm);
+    $('.pumpWatt').text(data.watt);
 
 
-    $('#pumpSource').text(data.source);
-    $('#pumpDestination').text(data.destination);
-    $('#pumpAction').text(data.action);
-    $('#pumpLength').text(data.length);
-    $('#pumpDriveState').text(data.driveState);
-    $('#pumpPpc').text(data.ppc);
+    $('.pumpSource').text(data.source);
+    $('.pumpDestination').text(data.destination);
+    $('.pumpAction').text(data.action);
+    $('.pumpLength').text(data.length);
+    $('.pumpDriveState').text(data.driveState);
+    $('.pumpPpc').text(data.ppc);
 
-    $('#pumpWatt').text(data.watt);
-    $('#pumpWatt').text(data.watt);
+    $('.pumpWatt').text(data.watt);
+
+
+    $('.unknown1').text(data.unknown1);
+    $('.unknown2').text(data.unknown2);
+    $('.unknown3').text(data.unknown3);
+    $('.unknown4').text(data.unknown4);
+
+    $('.timeCurrent').text(data.timeCurrent);
+
+    // $('.timeHours').text(data.timeHours);
+    // $('.timeMin').text(data.timeMin);
+    // $('.unknown4').text(data.unknown4);
+    // $('.unknown4').text(data.unknown4);
   });
 
-  // destination:  data[2],
-  // source:       data[3],
-  // action:       data[4],
-  // length:       data[5],
-  // state:         data[6], //4= off, 10 = on //power i think
-  // driveState:   data[7],
-  // ppc:          data[8],
-  // wattHighBit:  data[9],
-  // wattLowBit:   data[10],
-  // rpmHighBit:   data[11],
-  // rpmLowBit:    data[12],
-  // timerHighBit: data[15],
-  // timerLowBit:  data[18],
-  // timeHours:    data[19],
-  // timeMin:      data[20]
+  // var pumpData = {
+  //   destination:  data[2],
+  //   source:       data[3],
+  //   action:       data[4],
+  //   length:       data[5],
+  //   state:        data[6], //4= off, 10 = on //power i think
+  //   driveState:   data[7],
+  //   ppc:          data[8],
+  //   wattHighBit:  data[9],
+  //   wattLowBit:   data[10],
+  //   rpmHighBit:   data[11],
+  //   rpmLowBit:    data[12],
+  //   unknown1:     data[13],
+  //   unknown2:     data[14],
+  //   timerHighBit: data[15],
+  //   unknown3:     data[16],
+  //   unknown4:     data[17],
+  //   timerLowBit:  data[18],
+  //   timeHours:    data[19],
+  //   timeMin:      data[20]
+  // };
 });
 
 
