bc3f83aa136668c8a57ff883513beeb1eccd32cc
JeffreyStocker <kagesennin@gmail.com>
2018-02-03 22:27:42 -0800

    created a mockSerialPort to use on windows or if serial port has problems

--- server/serialPort.js
+++ server/serialPort.js
@@ -1,85 +1,86 @@
 const os = require('os'); //to get os version so i can install some testings
-const { pushPumpInfoToWebPages } = require ('./communications/outgoingSocketIO');
-const { convertToDecArray, parsePumpStatus } = require('./pump/helperFunctions.js');
-// const { pushPumpInfoToWebPages } = require ('./pump/sockets');
-var { showPumpStatusInConsole, acknowledgment } = require('./variables');
-var socketServer = require ('./server').socketServer;
+const { pushPumpInfoToWebPages } = require (process.env.NODE_PATH + '/server/communications/outgoingSocketIO');
+const { convertToDecArray, parsePumpStatus } = require(process.env.NODE_PATH + '/server/pump/helperFunctions.js');
+// const { pushPumpInfoToWebPages } = require (process.env.NODE_PATH + '/pump/sockets');
+var { showPumpStatusInConsole, acknowledgment } = require(process.env.NODE_PATH + '/server/variables');
+var socketServer = require (process.env.NODE_PATH + '/server/server.js').socketServer;
 
 
-var pumpData;
-
+var port;
 // var acknowledgment = require ('./messages');
 var init = exports.init = function () {
   //////// server, serial Port Functions //////////////////////
   if (os.platform() === 'win32') {
-    const SerialPort = require('serialport/test');
-    const MockBinding = SerialPort.Binding;
-    const portPath = 'COM_ANYTHING';
-    MockBinding.createPort(portPath, { echo: false, record: false });
-    port = exports.port = new SerialPort(portPath);
+    // const SerialPort = require('serialport/test');
+    // const MockBinding = SerialPort.Binding;
+    // const portPath = 'COM_ANYTHING';
+    // MockBinding.createPort(portPath, { echo: false, record: false });
+    // port = exports.port = new SerialPort(portPath);
 
-    port.on('open', () => {
-      // To pretend to receive data (only works on open ports)
-      port.binding.emitData(Buffer.from('Hi from my test!'));
-    });
-    // log received data
-    port.on('data', (data) => {
-      console.log('Received:\t', data.toString('utf8'));
-    });
+    // port.on('open', () => {
+    //   // To pretend to receive data (only works on open ports)
+    //   port.binding.emitData(Buffer.from('Hi from my test!'));
+    // });
+    // // log received data
+    // port.on('data', (data) => {
+    //   console.log('Received:\t', data.toString('utf8'));
+    //   console.log('crazy');
+
+    // });
     // port.write('message', () => {
     //   console.log('Write:\t\t Complete!');
     //   console.log('Last write:\t', port.binding.lastWrite.toString('utf8'));
     // });
 
+    port = exports.port = require (process.env.NODE_PATH + '/spec/mockSerialPort.js');
   } else {
     var SerialPort = require('serialport');
-    var port = exports.port = new SerialPort('/dev/ttyUSB0', function (err) {
+    port = exports.port = new SerialPort('/dev/ttyUSB0', function (err) {
       if (err) {
         return console.log('Error: ', err.message);
       }
       console.log ('Serial Port Created');
     });
-
-
-
-    // Switches the port into 'flowing mode'
-    port.on('data', function (data) {
-      // console.log ('data', data)
-      data = convertToDecArray (data);
-      if (data[4] === 7) {
-        pumpData = parsePumpStatus(data);
-        if (showPumpStatusInConsole) { console.log('Data Received:  [' + [... data] + ']'); }
-        // console.log ('Pump Data Updated')
-        try {
-          // pushPumpInfoToWebPages(pumpData);
-          // console.log (socketServer.emit)
-          socketServer.emit('pumpDataReturn', pumpData);
-        } catch (err) {
-          // console.log ('Error: Port:On: pushPUmpInfoToWebPages is not working')
-          console.log ('err: ', err);
-          console.log('Status Received:  [' + [... data] + ']');
-        }
-        acknowledgment.status = 'found';
-      } else if (acknowledgment.status === 'waiting For') {
-        var check = acknowledgment.isAcknowledgment(data);
-        // console.log('check', check)
-        acknowledgment.status = 'found';
-        console.log('Acknowledged:  [' + [... data] + ']');
-        // acknowledgment.message.push(data);
-        // acknowledgment.reset();
-      }
-
-      // else if (acknowledgment.status !== 'waiting For' || acknowledgment.isAcknowledgment(data) !== true) {
-      //   console.log ('not acknowledgment', data)
-      // }
-
-      else if (Array.isArray(data) === false) {
-        console.log(data);
-      } else {
-        console.log('Data Received:  [' + [... data] + ']');
-      }
-    });
   }
+
+  // Switches the port into 'flowing mode'
+  port.on('data', function (data) {
+    // console.log ('data', data)
+    data = convertToDecArray (data);
+    if (data[4] === 7) {
+      var pumpData = parsePumpStatus(data);
+      if (showPumpStatusInConsole) { console.log('Data Received:  [' + [... data] + ']'); }
+      // console.log ('Pump Data Updated')
+      try {
+        // pushPumpInfoToWebPages(pumpData);
+        // console.log (socketServer.emit)
+        socketServer.emit('pumpDataReturn', pumpData);
+      } catch (err) {
+        // console.log ('Error: Port:On: pushPUmpInfoToWebPages is not working')
+        console.log ('err: ', err);
+        console.log('Status Received:  [' + [... data] + ']');
+      }
+      acknowledgment.status = 'found';
+    } else if (acknowledgment.status === 'waiting For') {
+      var check = acknowledgment.isAcknowledgment(data);
+      // console.log('check', check)
+      acknowledgment.status = 'found';
+      console.log('Acknowledged:  [' + [... data] + ']');
+      // acknowledgment.message.push(data);
+      // acknowledgment.reset();
+    }
+
+    // else if (acknowledgment.status !== 'waiting For' || acknowledgment.isAcknowledgment(data) !== true) {
+    //   console.log ('not acknowledgment', data)
+    // }
+
+    else if (Array.isArray(data) === false) {
+      console.log(data);
+    } else {
+      console.log('Data Received:  [' + [... data] + ']');
+    }
+  });
+
 };
 
 init();
---
+++ spec/mockSerialPort.js
@@ -0,0 +1,49 @@
+const SerialPort = require('serialport/test');
+const MockBinding = SerialPort.Binding;
+const portPath = 'COM_ANYTHING';
+const helperFunctions = require (process.env.NODE_PATH + '/server/pump/helperFunctions.js');
+
+MockBinding.createPort(portPath, { echo: false, record: false });
+
+var lastMockInfo;
+port = module.exports = new SerialPort(portPath);
+
+// port.on('open', () => {
+//   // To pretend to receive data (only works on open ports)
+//   port.binding.emitData(Buffer.from('Hi from my test!'));
+// });
+// log received data
+// port.on('data', (data) => {
+//   console.log('Received:\t', data.toString('utf8'));
+//   console.log('crazy');
+// });
+
+var mockPortTimer = setInterval (() => {
+  var data = port.binding.lastWrite;
+  if (lastMockInfo === data) {
+    return;
+  } else {
+    lastMockInfo = data;
+    if (!data) { return; }
+    data = helperFunctions.convertToDecArray (data);
+    var postitionOfStartHeader = data.indexOf(165);
+    // console.log ('Mock Data Received: ', data);
+    flipDataInArray(data, postitionOfStartHeader + 2, postitionOfStartHeader + 3);
+    // console.log ('Mock Data Returned: ', data);
+    //must process data here
+    port.binding.emitData(Buffer.from(data));
+
+  }
+}, 10);
+
+var flipDataInArray = function (array, pos1, pos2) {
+  if (!Array.isArray(array) || typeof pos1 !== 'number' || typeof pos2 !== 'number') {
+    return;
+  }
+  if (array.length < pos1 || array.length < pos2) { return; }
+
+  var temp = array[pos1];
+  array[pos1] = array[pos2];
+  array[pos2] = temp;
+  return array;
+};
