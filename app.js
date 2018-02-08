b42f19f296a6655853f6afa676685f0bc60e65c6
Jeffrey Stocker <kagesennin@gmail.com>
2018-02-07 17:02:09 -0800

    refac: inital changes for more modular app.js file

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
 process.env.NODE_PATH = __dirname;
 
-// var config = require('./server/configureFile').init('./config.json');
 var configureFile = require(process.env.NODE_PATH + '/server/configureFile').init('default');
 var logger = require(process.env.NODE_PATH + '/server/logs/winston.js').init(configureFile.config.system.logs);
-configureFile.initLogging('system', logger)
+configureFile.initLogging('system', logger);
 
-var { statusRequestUpdateInverval } = require (process.env.NODE_PATH + '/server/variables');
-// var { timeBetweenQueueSending } = require (process.env.NODE_PATH + '/server/server');
-var { pumpGetStatus } = require (process.env.NODE_PATH + '/server/messages');
-var { queueLoopMain, addToQueue } = require (process.env.NODE_PATH + '/server/pump/queue');
-var incomingSockets = require (process.env.NODE_PATH + '/server/communications/incomingSocketIO');
+var useModular = false;
+
+if (useModular) {
 
 
-var timeBetweenQueueSending = 250; //intervel beteen when the queue sends off another message
+} else {
+  // var config = require('./server/configureFile').init('./config.json');
 
-//start main program
-setInterval(queueLoopMain, configureFile.config.system.queue.timeBetweenQueueSending);
-//starts routine pump status updates
-addToQueue(pumpGetStatus);
+  var { statusRequestUpdateInverval } = require (process.env.NODE_PATH + '/server/variables');
+  // var { timeBetweenQueueSending } = require (process.env.NODE_PATH + '/server/server');
+  var { pumpGetStatus } = require (process.env.NODE_PATH + '/server/messages');
+  var { queueLoopMain, addToQueue } = require (process.env.NODE_PATH + '/server/pump/queue');
+  var incomingSockets = require (process.env.NODE_PATH + '/server/communications/incomingSocketIO');
 
 
-setInterval( ()=> {
+  var timeBetweenQueueSending = 250; //intervel beteen when the queue sends off another message
+
+
+  //start main program
+  setInterval(queueLoopMain, configureFile.config.system.queue.timeBetweenQueueSending);
+  //starts routine pump status updates
   addToQueue(pumpGetStatus);
-}, statusRequestUpdateInverval); //gets pump status once every mintute
+
+
+  setInterval( ()=> {
+    addToQueue(pumpGetStatus);
+  }, statusRequestUpdateInverval); //gets pump status once every mintute
+}
 
 
 
