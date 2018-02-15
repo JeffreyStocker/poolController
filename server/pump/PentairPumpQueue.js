6b120d4156212b493416a162b19cb00874b6c493
Jeffrey Stocker <kagesennin@gmail.com>
2018-02-14 20:33:52 -0800

    feat: PentairPupQueue added winston logging and timer functions

--- server/pump/PentairPumpQueue.js
+++ server/pump/PentairPumpQueue.js
@@ -1,6 +1,9 @@
 var ActionQueue = require (process.env.NODE_PATH + '/server/Classes/ActionQueue.js');
-var messages = require (process.env.NODE_PATH + '/server/preBuiltMessages.js');
-var config = require (process.env.NODE_PATH + '/server/variables.js').config;
+var msg = require (process.env.NODE_PATH + '/server/preBuiltMessages.js');
+var serialPorts = require(process.env.NODE_PATH + '/server/serialPort_modular').ports;
+var helpers = require(process.env.NODE_PATH + '/server/helpers');
+
+var logger = () => {};
 
 var empty = function (context) {
   return pumpToLocal.byte;
@@ -19,13 +22,31 @@ var add = function (context) {
   return;
 };
 
-class PentairPumpQueues extends ActionQueue {
-  constructor () {
+module.exports.init = function (pumps = {}, logger = ()=>{}) {
+  module.exports.setLogger(logger);
+
+  var queues = module.exports.pentairPumpQueues = new PentairPumpQueues(logger);
+  var pumpsNames = Object.keys(pumps);
+  for (var pump of pumpsNames) {
+    try {
+      if (pump.enabled === true) {
+        queues.createQueue(pump);
+      }
+    } catch (err) {
+      logger ('system', 'warn', err);
+    }
+  }
+};
+
+
+class PentairPumpQueues {
+  constructor (logger = ()=>{}) {
     super ();
     this.queues = { };
-    this.timers = [];
+    this.timers = {};
     this.destination;
-    this.source;
+    this.source = 16;
+    this.logger = logger;
 
   }
 
@@ -33,9 +54,22 @@ class PentairPumpQueues extends ActionQueue {
 
   }
 
-  createQueue (queueName, type) {
-    if (!this.queues[queueName]) {
-      this.queues[queueName] = new Queue ({empty, start, add});
+  createQueue (queueInfo) {
+    var name, protocol, address;
+    if (!queueInfo.name || !queueInfo.communications) {
+      logger('system', 'warn', 'Could not create a new queue with this name: ' + queueInfo.name);
+      return;
+    }
+    try {
+      name = queueInfo.name;
+      protocol = queueInfo.communications.protocol;
+      address = msg.addresses['pump' + queueInfo.communications.address];
+    } catch (err) {
+      logger('system', 'warn', 'Could not create a new queue with this name: ' + queueInfo.name + ' | ' + err);
+    }
+
+    if (!this.queues[queueInfo.name]) {
+      this.queues[queueName] = new ActionQueue ({empty, start, add});
     }
     return this.queues[queueName];
   }
@@ -48,22 +82,27 @@ class PentairPumpQueues extends ActionQueue {
   }
 
   addMessageToQueue(queueName, message, callback = ()=>{}) {
+
     if (!this.queues[queueName]) { return null; }
 
     this.queues[queueName].add(message);
     callback();
   }
 
-  checkQueueForAcknowledgment (queueName, message) {
+  checkQueueForAcknowledgment (queueName, incomingMessage) {
     if (!this.queues[queueName]) {
       return null;
     }
-    var queuedMessage = this.peak();
-    var response = queuedMessage.tryAcknowledgment(message);
+    var queuedMessage = this.queues[queueName].peak();
+    var response = queuedMessage.tryAcknowledgment(incomingMessage);
     if (response) {
-
+      this.unshift();
+      this.logger('event', queuedMessage.logLevel, message.name + ': Received Return');
       return true;
-    } else {
+    } else if (response === -1) {
+      this.logger('event', 'info', message.name + ': timedout');
+    } else if (!response) {
+      this.logger('event', 'verbose', message.name + ': Received Return');
       return false;
     }
   }
@@ -72,4 +111,26 @@ class PentairPumpQueues extends ActionQueue {
     return this.queues;
   }
 
+  setTimer (queueName, name, time, func = ()=>{}) {
+    if (this.timers[queueName]) {
+      this.timers[queueName][name] = setInterval(func, time);
+    }
+  }
+  returnTimer (name) {
+    return this.timers[name];
+  }
+  cancelTimer (name) {
+    try {
+      clearInterval(this.timers[name]);
+    } catch (err) {
+    }
+  }
+
 }
+
+
+module.exports.setLogger = function (newLogger = logger) {
+  if (logger !== newLogger && typeof newLogger === 'function') {
+    logger = newLogger;
+  }
+};
