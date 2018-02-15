c0614375780abbd1236916f8a0cab177734d264f
Jeffrey Stocker <kagesennin@gmail.com>
2018-02-14 23:03:02 -0800

    Feat: Added a function to test modularity Comment: Added some commented functions to handle exiting app Feat: Added winston logger function

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 process.env.NODE_PATH = __dirname;
 
 var configureFile = require(process.env.NODE_PATH + '/server/configureFile').init('default');
-var logger = require(process.env.NODE_PATH + '/server/logs/winston.js').init(configureFile.config.system.logs);
+var logger = require(process.env.NODE_PATH + '/server/logging/winston.js').init(configureFile.config.system.logs);
 configureFile.initLogging('system', logger);
 
+//temp
 var useModular = false;
 
 if (useModular) {
-
+  var serialPaths = configureFile.config.system.communications && configureFile.config.system.communications.rs485.ports;
+  require(process.env.NODE_PATH + '/server/serialPort_modular.js').init(serialPaths);
+  var incomingSockets = require (process.env.NODE_PATH + '/server/communications/incomingSocketIO');
 
 } else {
   // var config = require('./server/configureFile').init('./config.json');
 
-  var { statusRequestUpdateInverval } = require (process.env.NODE_PATH + '/server/variables');
+  var { statusRequestUpdateInverval, statusTimers } = require (process.env.NODE_PATH + '/server/variables');
   // var { timeBetweenQueueSending } = require (process.env.NODE_PATH + '/server/server');
   var { pumpGetStatus } = require (process.env.NODE_PATH + '/server/messages');
   var { queueLoopMain, addToQueue } = require (process.env.NODE_PATH + '/server/pump/queue');
@@ -28,8 +31,10 @@ if (useModular) {
   addToQueue(pumpGetStatus);
 
 
-  setInterval( ()=> {
+  statusTimers = setInterval( ()=> {
     addToQueue(pumpGetStatus);
+    // console.log (pumpToLocal);
+    // addToQueue(pumpToLocal);
   }, statusRequestUpdateInverval); //gets pump status once every mintute
 }
 
@@ -38,3 +43,34 @@ if (useModular) {
 // process.on('uncaughtException', function(err) {
 //   console.log('Caught exception: ' + err);
 // });
+
+
+
+// process.stdin.resume();//so the program will not close instantly
+
+// var exitHandler = function(options, err) {
+//   if (options.cleanup) { console.log('clean'); }
+//   if (err) { console.log(err.stack); }
+//   let packet = Buffer.from([165, 0, 96, 16, 4, 1, 0, 1, 26, 1, 53]); //adds header, checksum and converts to a buffer
+
+//   port.write(packet, function(err) {
+//     if (err) {
+//       console.log('Error on write: ', err.message);
+//     }
+//     callback(0);
+//     if (options.exit) { process.exit(); }
+//   });
+// };
+
+// //do something when app is closing
+// process.on('exit', exitHandler.bind(null, {cleanup: true}));
+
+// //catches ctrl+c event
+// process.on('SIGINT', exitHandler.bind(null, {exit: true}));
+
+// // catches "kill pid" (for example: nodemon restart)
+// process.on('SIGUSR1', exitHandler.bind(null, {exit: true}));
+// process.on('SIGUSR2', exitHandler.bind(null, {exit: true}));
+
+// //catches uncaught exceptions
+// process.on('uncaughtException', exitHandler.bind(null, {exit: true}));
