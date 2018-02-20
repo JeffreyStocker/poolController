3fb18b4e4f7a607373bc533aea7b8828008fa103
JeffreyStocker <kagesennin@gmail.com>
2018-02-20 15:15:04 -0800

    fixed: location data of modules

--- server/communications/serialPortInit.js
+++ server/communications/serialPortInit.js
@@ -3,13 +3,13 @@ var configureFile = require(process.env.NODE_PATH + '/server/configureFile').con
 var logger = require (process.env.NODE_PATH + '/server/logging/winston').sendToLogs;
 const { convertToDecArray, parsePumpStatus, isStatusMessage } = require(process.env.NODE_PATH + '/server/equipment/pentair/helperFunctions.js');
 
-var processIncomingSerialPortData = require(process.env.NODE_PATH + '/server/serialPort.js').processIncomingSerialPortData;
+var processIncomingSerialPortData = require(process.env.NODE_PATH + '/server/communications/serialPort.js').processIncomingSerialPortData;
 
 module.exports.init = function () {
   var serialPaths = configureFile.system.communications && configureFile.system.communications.rs485.ports;
-  var sp = require(process.env.NODE_PATH + '/server/serialPort_modular.js');
+  var sp = require(process.env.NODE_PATH + '/server/communications/serialPort_modular.js');
   sp.init(serialPaths, logger);
-  sp.setGroupOfTriggers('pump1', {
+  sp.setGroupOfTriggers('/dev/ttyUSB0', {
     'data': processIncomingSerialPortData
   });
 };
