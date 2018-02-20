05ec72e71d9b0788b25111a6cb9237b391c66d13
JeffreyStocker <kagesennin@gmail.com>
2018-02-20 15:17:38 -0800

    Refac: to reduce naming

--- server/equipment/pentair/queue.js
+++ server/equipment/pentair/queue.js
@@ -4,18 +4,7 @@ const { port } = require(process.env.NODE_PATH + '/server/communications/serialP
 var { queueLoopMain_InUse, showPumpStatusInConsole, acknowledgment } = require(process.env.NODE_PATH + '/server/variables');
 var logger = require (process.env.NODE_PATH + '/server/logging/winston').sendToLogs;
 
-const { prefix,
-  shortPrefix,
-  pumpToRemote,
-  pumpToLocal,
-  pumpExternal_Speed4,
-  pumpExternal_Speed3,
-  pumpExternal_Speed2,
-  pumpExternal_Speed1,
-  pump_Off,
-  pumpGetStatus,
-  pump_PowerOn,
-  pump_PowerOff } = require(process.env.NODE_PATH + '/server/equipment/pentair/preBuiltMessages');
+const PentairMessages = require(process.env.NODE_PATH + '/server/equipment/pentair/PentairMessages');
 
 var queueMessagesToSend = [];
 var queueLoopMain_InUse = false;
@@ -82,17 +71,17 @@ var addStatusToQueue = module.exports.addStatusToQueue = function () {
 };
 
 
-var addToQueue = module.exports.addToQueue = function (packet) {
+var addToQueue = module.exports.addToQueue = function (message) {
   //add a pcket to the queue
   //if queue is empty, adds packets to set pump to remote in beginning, and set pump to local at end of queue
   //this way the pump should only be set to remote when sending information
 
   if (queueMessagesToSend.length === 0) {
     // queueMessagesToSend.push(pumpToRemote);   //i guess i don't really need SetToRemote
-    queueMessagesToSend.push(packet);
-    queueMessagesToSend.push(pumpToLocal);
+    queueMessagesToSend.push(message);
+    queueMessagesToSend.push(PentairMessages.defaultPumpControlPanelMessage('local'));
   } else {
-    queueMessagesToSend[queueMessagesToSend.length - 1] = packet;
+    queueMessagesToSend[queueMessagesToSend.length - 1] = message;
     // ok i guess i do, it appears that while you can send commands without the SetToRemote, if you fail to have SetToLocal, the pump does not come out of remote  --or it might be an another annoying artiface from nodejs-PentairControlSystem
   }
 };
@@ -100,7 +89,7 @@ var addToQueue = module.exports.addToQueue = function (packet) {
 
 var sendCommand = module.exports.sendCommand = function (message, name = 'unknown') { //sendCommandViaArrayOfBytes
   var packet, byteArray;
-  if (!message) { return logger('events', 'error', 'sendCommand: Message is invalid:' + message); }
+  if (!message) { return logger('events', 'error', 'sendCommand: Message is undefined:' + message); }
 
   if (typeof message === 'object' && Array.isArray(message) === false) {
     byteArray = message.packet;
@@ -121,7 +110,7 @@ var sendCommand = module.exports.sendCommand = function (message, name = 'unknow
     ) {
     } else { //ignore logging of the status messages
       // console.log('Sent Command: ' + name, ': [' + [...packet] + ']');
-      logger('events', 'verbose', 'Sent Command: ' + name, ': [' + [...packet] + ']');
+      logger('events', 'verbose', 'Sent Command: ' + name, ': [' + [...message.packet] + ']');
     }
   });
 };
