300ed6ea659627a0007753fe9abd2e8cd54b61cf
JeffreyStocker <kagesennin@gmail.com>
2018-02-20 15:13:19 -0800

    cleaned up code Bug: fixed running undefined actions

--- server/Classes/ActionQueue.js
+++ server/Classes/ActionQueue.js
@@ -1,13 +1,14 @@
 var TwinLinkedStack = require(process.env.NODE_PATH + '/server/Classes/TwinLinkedStack');
 
-class ActionQueue extends TwinLinkedStack {
+module.exports = class ActionQueue extends TwinLinkedStack {
   constructor (config = {actions: {}, data: undefined, } ) {
-    super(data);
+    super();
 
     if (typeof config !== 'object' ) { throw 'Config mush be set to an action'; }
+
     this._InUse = false;
     this._started = false;
-    this._actions = config.actions;
+    this._actions = config.actions || {};
   }
 
   runAction(name, context = this) {
@@ -25,30 +26,22 @@ class ActionQueue extends TwinLinkedStack {
   pull () {
     if (this._InUse && this.length === 0) {
       this._InUse = false;
-      if (this._actions.empty !== undefined) {
-        return this.runAction('empty', this);
-      } else {
-        return undefined;
-      }
+      return this.runAction('empty', this);
     } else if (this._InUse) {
       return this.shift();
     } else if (!this._InUse && this.length === 0) {
       return undefined;
     } else if (!this._InUse) {
       this._InUse = true;
-      // console.log ('start', this._startAction)
-      if (this._actions.start !== undefined) {
-        return this.runAction('start', this);
-      }
-      return this.shift();
+      return this.runAction('start', this);
     }
     return null;
   }
 
   add(message) {
     this.push(message);
-    if (this._actions.start !== undefined) {
-      return this.runAction('add');
-    }
+    return this.runAction('add');
+    // if (this._actions.start !== undefined) {
+    // }
   }
-}
+};
