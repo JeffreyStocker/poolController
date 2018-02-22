0735cceae60ec9f18c518920f5442586fbdd2bab
JeffreyStocker <kagesennin@gmail.com>
2018-02-21 18:02:03 -0800

    Refact: PrebuildMessages, will delete now, all in PentairMessages

--- server/equipment/pentair/preBuiltMessages.js
+++ server/equipment/pentair/preBuiltMessages.js
@@ -1,83 +1,141 @@
-/////// packet Information /////////////////////////
-/*
-speed #, Higher # higher priority
-soler 4
-slow speed 3
-waterfall speed 2
-heater/spa speed 1
-*/
-var defaultMessages = {
-  prefix: [255, 0, 255, 165, 0],
-  shortPrefix: [255, 0, 255],
-  pumpToRemote:        {name: 'Set Pump to Remote', byte: [165, 0, 96, 16, 4, 1, 255],          buffer: Buffer.from('a50060100401ff0219', 'hex'),       hex: 'a50060100401ff0219'},       byteWithChecksum: [165, 0, 96, 16, 4, 1, 255, 2, 25], //intellicom set pump to remote  //works
-  pumpToLocal:         {name: 'Set Pump to Local',  byte: [165, 0, 96, 16, 4, 1, 0],            buffer: Buffer.from('a5006010040100011a', 'hex'),       hex: 'a5006010040100011a'},       byteWithChecksum: [165, 0, 96, 16, 4, 1, 0, 1, 26],
-  pumpExternal_Speed4: {name: 'Exteral Speed 4',    byte: [165, 0, 96, 16, 1, 4, 3, 33, 0, 32], buffer: Buffer.from('a5006010010403210020015e', 'hex'), hex: 'a5006010010403210020015e'}, byteWithChecksum: [165, 0, 96, 16, 1, 4, 3, 33, 0, 32, 1, 94], //intellicom use external command i think 4 (highest his is solar  priority) (possibley with 1 min timer?)
-  pumpExternal_Speed3: {name: 'Exteral Speed 3',    byte: [165, 0, 96, 16, 1, 4, 3, 33, 0, 24], buffer: Buffer.from('a50060100104032100180156', 'hex'), hex: 'a50060100104032100180156'}, byteWithChecksum: [165, 0, 96, 16, 1, 4, 3, 33, 0, 24, 1, 86], //slow Speed
-  pumpExternal_Speed2: {name: 'Exteral Speed 2',    byte: [165, 0, 96, 16, 1, 4, 3, 33, 0, 16], buffer: Buffer.from('a5006010010403210010014e', 'hex'), hex: 'a5006010010403210010014e'}, byteWithChecksum: [165, 0, 96, 16, 1, 4, 3, 33, 0, 16, 1, 78], //waterfall
-  pumpExternal_Speed1: {name: 'Exteral Speed 1',    byte: [165, 0, 96, 16, 1, 4, 3, 33, 0, 8],  buffer: Buffer.from('a50060100104032100080146', 'hex'), hex: 'a50060100104032100080146'}, byteWithChecksum: [165, 0, 96, 16, 1, 4, 3, 33, 0, 8, 1, 70], //spa
-  pumpExternal_Off:    {name: 'Exteral OFF',        byte: [165, 0, 96, 16, 1, 4, 3, 33, 0, 0],  buffer: Buffer.from('a5006010010403210000013e', 'hex'), hex: 'a5006010010403210000013e'}, byteWithChecksum: [165, 0, 96, 16, 1, 4, 3, 33, 0, 0, 1, 62],
-  pumpGetStatus:       {name: 'Get Pump Status',    byte: [165, 0, 96, 16, 7, 0],               buffer: Buffer.from([165, 0, 96, 16, 7, 0, 1, 28]),     hex: 'A50961670128'},             byteWithChecksum: [165, 0, 96, 16, 7, 0, 1, 28],
-  pump_PowerOn:        {name: 'Set Power On',       byte: [165, 0, 96, 16, 6, 1, 10],           buffer: Buffer.from('A500601006010A0126', 'hex'),       hex: 'A500601006010A0126',        byteWithChecksum:[165, 0, 96, 16, 6, 1, 10, 1, 38]},
-  pump_PowerOff:       {name: 'Set Power Off',      byte: [165, 0, 96, 16, 6, 1, 4],            buffer: Buffer.from('A50060100601040120', 'hex'),       hex: 'A50060100601040120',        byteWithChecksum:[165, 0, 96, 16, 6, 1, 4, 1, 32]},
-  runFilter:           {name: 'Speed Filter',       byte: [165, 0, 96, 16, 5, 1, 0]},
-  runManual:           {name: 'Speed Manual',       byte: [165, 0, 96, 16, 5, 1, 1]},
-  pumpSpeed1:          {name: 'Speed 1',            byte: [165, 0, 96, 16, 5, 1, 2]},
-  pumpSpeed2:          {name: 'Speed 2',            byte: [165, 0, 96, 16, 5, 1, 3]},
-  pumpSpeed3:          {name: 'Speed 3',            byte: [165, 0, 96, 16, 5, 1, 4]},
-  pumpSpeed4:          {name: 'Speed 4',            byte: [165, 0, 96, 16, 5, 1, 5]},
-  feature1:            {name: 'feature 1',          byte: [165, 0, 96, 16, 5, 1, 6]},
-  saveAndRunExternal1: {name: 'Save & Run External 1', byte: [165, 0, 96, 16, 1, 4, 2, 196]}, //needs a speed high and low bit added on the end before the checksum
-};
+var { Message, defaultMessages } = require (process.env.NODE_PATH + '/server/equipment/pentair/PentairMessages.js');
 
 module.exports = {
-  returnDefaultMessage(preBuiltMessage, destination = 96, source = 16, options = {name: null, log: true} ) {
-    var log;
+  returnDefaultMessage(preBuiltMessage, destination = 96, options = {name: null, logLevel: true} ) {
+    var logLevel, name;
     if (!options) {
       name = defaultMessages[preBuiltMessage].name;
-      log = true;
+      logLevel = true;
     } else {
       name = options.name || defaultMessages[preBuiltMessage].name;
-      (options.log !== undefined && typeof options.log === 'boolean') ? log = options.log : log = true;
+      (options.logLevel !== undefined && typeof options.logLevel === 'boolean') ? logLevel = options.logLevel : logLevel = true;
     }
     var message = defaultMessages[preBuiltMessage].byte.slice();
     message[2] = destination;
-    message[3] = source;
-    return { name, message, log };
+    return new Message (message, name, {logLevel});
   },
 
-  buildMessage() {
-
+  defaultStatusMessage(destination = 96, callback) {
+    var message = new Message (defaultMessages.pumpGetStatus.byte, defaultMessages.pumpGetStatus.name, {logLevel: 'debug'}, callback);
+    message.setDestination(destination);
+    return message;
   },
 
-  returnDefaultByte (preBuiltMessage, destination = 96, source = 16, name) {
-    name = name || module.exports[preBuiltMessage].name;
-    var message = module.exports[preBuiltMessage].byte.slice();
-    message[2] = destination;
-    message[3] = source;
-    return { name, byte: message };
+  defaultPumpPowerMessage (powerState, callback) {
+    var defaultMessage;
+    if (powerState === 'on') {
+      defaultMessage = defaultMessages.pump_PowerOn;
+    } else if (powerState === 'off') {
+      defaultMessage = defaultMessages.pump_PowerOff;
+    } else {
+      throw new Error('Error: In order to change the pump Power state, you need to enter true/false or on/off');
+    }
+    return new Message(defaultMessage.byte, defaultMessage.name, callback);
   },
 
-  addresses: {
-    Chlorinator: [2, 16], // 	Chlorinator (This is a different type of packet)
-    Broadcast: 15,
-    Pool_controller: 16, // 	Pool controller (EasyTouch, intellicom, et al)
-    RemoteWiredController: 32,
-    RemoteWirelessController: 34, 	//(Screen Logic, or any apps that connect to it, like this one)
-    Pump1: 96,
-    Pump2: 97,
-    Pump3: 98,
-    Pump4: 99,
-    Pump5: 100,
-    Pump6: 101,
-    Pump7: 102,
-    Pump8: 103,
-    Pump9: 104,
-    Pump10: 105,
-    Pump11: 106,
-    Pump12: 107,
-    Pump13: 108,
-    Pump14: 109,
-    Pump15: 110,
-    Pump16: 111,
+  defaultPumpControlPanelMessage (powerState, callback) {
+    var defaultMessage;
+    if (powerState.toLowerCase() === 'remote') {
+      return new Message (defaultMessages.pumpToRemote.byte, defaultMessages.pumpToRemote.name, callback);
+      defaultMessage = defaultMessages.pumpToRemote;
+    } else if (powerState.toLowerCase() === 'local') {
+      return new Message (defaultMessages.pumpToLocal.byte, defaultMessages.pumpToLocal.name, callback);
+      defaultMessage = defaultMessages.pumpToLocal;
+    } else {
+      throw new Error('Error: In order to change the pump Power state, you need to enter local or remote');
+    }
+  },
+
+  defaultIntellicomMessage (speed = 0, callback = ()=>{}) {
+    if (speed === 0) {
+      return new Message (defaultMessages.pumpExternal_Off.byte, defaultMessages.pumpExternal_Off.name, callback);
+    } else if (speed > 0 && speed < 5) {
+      var speedname = 'pumpExternal_Speed' + speed;
+      return new Message (defaultMessages[speedname].byte, defaultMessages[speedname].name, callback);
+    }
   }
+
+};
+
+
+
+
+
+//not working
+var messagePumpSpeed = function (rpm, program, destination = 96, callback = ()=>{}) {
+  var possibleSubcommands = {
+    0: 33,
+    1: 39,
+    2: 40,
+    3: 41,
+    4: 42,
+  };
+  var packet = {};
+  var action = 1; //1= set speed mode, unsure if other modes
+  var length = 4; //for speed, i don't think this changes from 4,
+  var command = 33; //3= run program , 2= run/save? at speed????
+  var subcommand = possibleSubcommands[program]; //33 = run program xx / turn off pump, 39-42, save P1-4 as xx, 43 set pump timer for xx min || command =2; subcommand - 96 or 196 then might be linked to speed 1
+  var commandHighBit = returnHighBit (rpm); //changes, either RPM high or timer HH or MM
+  var commandLowBit = returnLowBit (rpm); //changes either rpm low, or timer MM
+  packet['byte'] = [165, 0, destination, source, action, length, command, subcommand, commandHighBit, commandLowBit];
+  packet.name = 'Run Pump Program: ' + program + ' at ' + rpm;
+  return new Message(packet.byte, packet.name, callback);
+};
+
+//// not sure if going to keep these
+var convertPacket = function() {
+  if (typeof packet === 'string') {
+    let defaultlPacket = this.returnDefaults(packet);
+    if (!!defaultPacket) {
+      this.packet = defaultPacket.packet;
+      this.name = defaultPacket.name;
+      this.originalPacket = defaultPacket.packet;
+    } else {
+
+    }
+  } else if (Array.isArray(packet)) {
+    this.packet = this.convertToDefaultlPacket(packet);
+    this.name = name;
+    this.originalPacket = packet;
+  }
+
+};
+
+var convertToDefaultMessage = function (message) {
+  // var type = this.findType(message);
+  var type, convertedMessage;
+  try {
+    type = message.constructor.name;
+  } catch (err) {
+    return;
+  }
+
+  switch (type) {
+  case 'buffer': {
+    convertedMessage = this.returnArrayFromBuffer(message);
+    break;
+  }
+  case 'array': {
+    this.isHexArray === true ? convertedMessage = this.convertHexArrayToByteArray (message) : convertedMessage = message;
+    break;
+  }
+  case 'string': {
+    if (message.length % 2 !== 0) { throw 'A message string must be divisable by 2'; }
+    if (this.isHexString(message)) {
+      convertedMessage = this.sliceStringByRecurringAmounts(message, 2);
+      convertedMessage = this.convertHexArrayToByteArray(convertedMessage);
+    } else {
+      convertedMessage = this.sliceStringByRecurringAmounts(message, 2);
+    }
+    break;
+  }
+  case 'number': {
+    if (message.length % 2 !== 0) { throw 'A message string must be diviable by 2'; }
+    convertedMessage = this.sliceStringByRecurringAmounts(message.toString(), 2);
+    break;
+  }
+  default:
+    break;
+  }
+  return convertedMessage;
 };
