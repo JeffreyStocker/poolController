a94beb7bd7db018e03d2ce9868d7f743256487ea
JeffreyStocker <kagesennin@gmail.com>
2018-02-27 13:49:33 -0800

    Refact: to make sure that certain events happened sync

--- server/communications/serialPortInit.js
+++ server/communications/serialPortInit.js
@@ -2,13 +2,26 @@
 var configureFile = require(process.env.NODE_PATH + '/server/configureFile').config;
 var logger = require (process.env.NODE_PATH + '/server/logging/winston').sendToLogs;
 const { convertToDecArray, parsePumpStatus, isStatusMessage } = require(process.env.NODE_PATH + '/server/equipment/pentair/helperFunctions.js');
-
+var Promise = require('bluebird');
 var processIncomingSerialPortData = require(process.env.NODE_PATH + '/server/communications/serialPort.js').processIncomingSerialPortData;
 
-module.exports.init = function () {
+
+module.exports.init = async function () {
   var serialPaths = configureFile.system.communications && configureFile.system.communications.rs485;
   var sp = require(process.env.NODE_PATH + '/server/communications/serialPort_modular.js');
-  var waitTOFinish = sp.init(serialPaths, logger);
+  sp.setLogger(logger);
+  // var waitTOFinish = sp.init(serialPaths, logger);
+
+  var results = [];
+  for (var port of serialPaths) {
+    if (port.type === 'rs485'){
+      results.push(sp.newSerialPort(port.name, port.hardwareAddress));
+    }
+  }
+  results = await Promise.all(results)
+  return sp
+
+
   // sp.setGroupOfTriggers('/dev/ttyUSB0', {
   //   'data': processIncomingSerialPortData
   // });
