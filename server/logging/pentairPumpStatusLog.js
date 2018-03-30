4dc556fb260eb6c3330c713d4b6caba13a8724ab
Jeffrey Stocker <kagesennin@gmail.com>
2018-03-30 16:05:14 -0700

    Added Moments and adjusted to use _id as the timeStamp

--- server/logging/pentairPumpStatusLog.js
+++ server/logging/pentairPumpStatusLog.js
@@ -1,13 +1,30 @@
 var PentairMessage = require('./../equipment/pentair/PentairMessages.js');
 var PouchDB = require('pouchdb');
 var db = new PouchDB('./database/power');
-// var moment = require('moment');
+var Moment = require('moment');
 PouchDB.plugin(require('pouchdb-find'));
 
 var logStatus, currentMin;
 var currentLogs = [];
 var interval = 5;
 
+var timeInMilli = function (type, amount) {
+  if (typeof type !== 'string' && typeof amount !== 'number') { return NaN; }
+  if (type === 'hour') {
+    return amount * 1000 * 60 * 60;
+
+  } else if (type === 'minute') {
+    return amount * 1000 * 60;
+
+  } else if (type === 'second') {
+    return amount * 1000;
+
+  } else if (type === 'day') {
+    return amount * 1000 * 60 * 60 * 24;
+  } else {
+    return NaN;
+  }
+};
 
 var init = function (invervalInMin = 5) {
   var now = new Date();
@@ -67,6 +84,26 @@ var log = function (parsedPumpData, pumpName = 'pump1') {
 };
 
 
+var past = function (time, amount) {
+  if (typeof time === 'string' && typeof amount === 'number') {
+    if (time === 'months') {
+      return findBetweenTime(moment().subtract(amount, 'months').toDate());
+    } else if (time === 'years') {
+      return findBetweenTime(moment().subtract(amount, 'years').toDate());
+    } else if (time === 'days') {
+      return findBetweenTime(moment().subtract(amount, 'days').toDate());
+
+    } else if (time === 'hours') {
+      return findBetweenTime(moment().subtract(amount, 'hours').toDate());
+    } else {
+    }
+  } else if (time.constructor.name === 'Moment') {
+    return findBetweenTime(time.toDate());
+  } else {
+    throw new Error('Time should be a string (hours, months, days, years or a Moment object and amount should be a number');
+  }
+};
+
 var findBetweenTime = function (startTime = new Date(), endTime = new Date()) {
   return new Promise ((resolve, revoke) => {
     if (startTime.constructor.name !== 'Date' || endTime.constructor.name !== 'Date') { revoke(new Error('startTime and endTime must be Date Objects')); }
@@ -77,7 +114,7 @@ var findBetweenTime = function (startTime = new Date(), endTime = new Date()) {
     }
     db.find({
       selector: {
-        timeStamp: { $lte: startTime, $gte: endTime }
+        _id: { $lte: startTime, $gte: endTime }
       }
     }, (err, docs) => {
       if (err) {
@@ -99,16 +136,36 @@ var findBetweenTimePromise = function(startTime = new Date(), endTime = new Date
   }
   return db.find({
     selector: {
-      timeStamp: { $lte: startTime, $gte: endTime }
+      _id: { $lte: startTime, $gte: endTime }
     }
   });
 };
 
 
 var insertAtTime = function (data) {
-  if (!data || typeof data !== 'object') { return new Error ('Data to insert into database should be a object'); }
-  data.timeStamp = new Date();
-  return db.post(data);
+  return new Promise ((resolve, revoke) => {
+    if (!data || typeof data !== 'object') { return new Error ('Data to insert into database should be a object'); }
+    data._id = new Date();
+    db.post(data)
+      .then(resolveData => {
+        resolve(resolveData);
+      })
+      .catch(err => {
+        if (err.code === 202) {
+          if (!errorCount) {
+            var errorCount = 0;
+          } else if (errorCount > 5) {
+            err.secondErrorMessage = 'Attempted to increase the _id count by 1 too may times';
+            revoke (err);
+          }
+          errorCount ++;
+          data._id++;
+          resolve(insertAtTime(data));
+        } else {
+          revoke (err);
+        }
+      });
+  });
 };
 
 
@@ -121,6 +178,8 @@ var allDocs = function () {
   return db.allDocs({});
 };
 
+insertAtTime({name: 'test'});
+
 
 module.exports = {
   insertAtTime,
@@ -128,5 +187,12 @@ module.exports = {
   findBetweenTime,
   init,
   shrink,
-  log
+  log,
+  shrinkAndSave
 };
+
+
+count()
+  .then(data => {
+    console.log(data);
+  });
