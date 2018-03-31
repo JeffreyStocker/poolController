37c888991962544d948e7bf27eb4d28860d93f0d
Jeffrey Stocker <kagesennin@gmail.com>
2018-03-31 15:52:06 -0700

    format: moved data in pumpStatus to higher up in componenet

--- public/vue/index.js
+++ public/vue/index.js
@@ -80,21 +80,21 @@ Vue.component('groupOfButtons', {
 });
 
 Vue.component('pumpStatus', {
+  data: function () {
+    return {
+      message,
+      showExtended: false};
+  },
   props: ['pumpData'],
   template: `
-  <div class='center' v-on:click='showExtended=!showExtended'>
+  <div class='center' v-on:dblclick='showExtended=!showExtended'>
 			<div>Pump Info</div>
 			<pumpStatusData v-bind:pumpData='pumpData'></pumpStatusData>
 			<pumpStatusDataExtended v-bind:pumpData='pumpData' v-show='showExtended'></pumpStatusDataExtended>
 
 			<div class="message" id="message"> {{ message }}</div>
 		</div>
-  `,
-  data: function () {
-    return {
-      message,
-      showExtended: false};
-  }
+  `
 });
 
 Vue.component('pumpStatusData', {
