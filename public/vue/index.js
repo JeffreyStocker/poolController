63a40c9e752ab93e08405c21853791c4d419b6b8
Jeffrey Stocker <kagesennin@gmail.com>
2018-04-07 11:21:47 -0700

    uncommented index.js

--- public/vue/index.js
+++ public/vue/index.js
@@ -1,36 +1,36 @@
-// import Vue from 'vue';
-// import pumpData from '/socket.js';
-// import buttonx from '/components/buttonx.js'
-// import groupOfButtons from '/components/GroupOfButtons.js'
-// import PumpStatus from '/components/PumpStatus.js'
-// import PumpStatusData from '/components/PumpStatusData.js'
+import Vue from 'vue';
+import pumpData from '/socket.js';
+import buttonx from '/components/buttonx.js';
+import groupOfButtons from '/components/GroupOfButtons.js';
+import PumpStatus from '/components/PumpStatus.js';
+import PumpStatusData from '/components/PumpStatusData.js';
 
-// var socket = io.connect();
+var socket = io.connect();
 
-// var sendDataToServer, pumpData;
+var sendDataToServer;
 
-// var message = '';
+var message = '';
 
 
 
-// var app = new Vue({
-//   el: '#app',
-  // data: {
-    // message: 'Hello Vue!',
-    // items: [[{name: 'test'}], [{name: 'test2'}], [{name: 'test3'}]],
-    // pumpData: pumpData,
-    // showExtended: false
-  // },
-  // template: `
-  // <div>
-  //   <pumpStatus v-bind:pumpData='pumpData'></pumpStatus>
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+    items: [[{name: 'test'}], [{name: 'test2'}], [{name: 'test3'}]],
+    pumpData: pumpData,
+    showExtended: false
+  },
+  template: `
+  <div>
+    <pumpStatus v-bind:pumpData='pumpData'></pumpStatus>
 
-  //   <groupOfButtons
-  //     v-for="(item, num) in items"
-  //     v-bind:group="item"
-  //     v-bind:key="num"
-  //     >
-  //   </groupOfButtons>
-  // </div>
-  // `
-// });
+    <groupOfButtons
+      v-for="(item, num) in items"
+      v-bind:group="item"
+      v-bind:key="num"
+      >
+    </groupOfButtons>
+  </div>
+  `
+});
