4be77507233f12165c694c0b98409f18b732b5cf
Jeffrey Stocker <kagesennin@gmail.com>
2018-04-08 22:39:38 -0700

    implemented pumpbuttons

---
+++ src/vue/components/ButtonPump.vue
@@ -0,0 +1,24 @@
+<script>
+  import { socket } from '../socket.js';
+
+  export default {
+    props: ['onClickData', 'name', 'onClick'],
+    methods: {
+      button: function () {
+        if (this.onClick && typeof this.onClick === 'function') {
+          socket.emit(...this.onClickData, this.onClick);
+        } else {
+          console.log (this.name, ': Button Clicked but No Event');
+        }
+      }
+    }
+  }
+</script>
+
+<template>
+  <button
+    class="btn"
+    v-on:click="button"
+  > {{name}}
+  </button>
+</template>
---
+++ src/vue/components/GroupOfPumpButtons.vue
@@ -0,0 +1,47 @@
+<script>
+  import ButtonPump from './ButtonPump.vue';
+
+  /*
+    var test = {
+      name: [data1, data2, data3]
+      }
+    v-for="(val, key) in test"
+    v-bind:key="key"
+    v-bind:name="key"
+    v-bind:methodData="val"
+  */
+  var sendMessage = function() {
+
+  }
+  export default {
+    computed: {},
+    components: { ButtonPump },
+    data: function () {
+      return {
+      }
+    },
+    props: ['buttons', 'setMessage', 'title'],
+    methods: {
+      send: function () {
+      }
+    },
+    watch: {}
+  }
+</script>
+
+<template>
+  <div class="col-sm-2 center">
+    {{title || 'Unknown'}}
+    <ButtonPump
+      v-for="(data, name) in buttons"
+      v-bind:key="name"
+      v-bind:name="name"
+      v-bind:onClickData="data"
+      v-bind:onClick="setMessage"
+      >
+    </ButtonPump>
+  </div>
+</template>
+
+<style>
+</style>
