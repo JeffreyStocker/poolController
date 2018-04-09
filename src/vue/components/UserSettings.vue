5423d28e067cd6c4c150609a94bbea6af39637f0
Jeffrey Stocker <kagesennin@gmail.com>
2018-04-09 11:24:46 -0700

    init UserSettings.vue

---
+++ src/vue/components/UserSettings.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    computed: {},
+    components: {},
+    data: function () {
+      return {}
+    },
+    props: [],
+    methods: {},
+    watch: {}
+  }
+</script>
+
+<template>
+
+</template>
+
+<style>
+</style>
