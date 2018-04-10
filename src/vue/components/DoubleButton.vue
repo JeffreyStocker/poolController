c3628d7cc75de71088b843c38776fc1487fe71a3
Jeffrey Stocker <kagesennin@gmail.com>
2018-04-10 10:39:38 -0700

    init started DoubleButton.vue

---
+++ src/vue/components/DoubleButton.vue
@@ -0,0 +1,26 @@
+<script>
+  import ButtonPump from './ButtonPump.vue';
+  export default {
+    computed: {},
+    components: { ButtonPump },
+    data: function () {
+      return {}
+    },
+    props: ['buttons', 'setMessage'],
+
+  }
+</script>
+
+<template>
+  <span>
+    <ButtonPump :onClick="setMessage">
+
+    </ButtonPump>
+    <ButtonPump>
+
+    </ButtonPump>
+  </span>
+</template>
+
+<style>
+</style>
