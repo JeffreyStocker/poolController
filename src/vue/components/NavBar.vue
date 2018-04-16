4a43342efe713e6e8e4b500165c486bd7c5c9a1d
Jeffrey Stocker <kagesennin@gmail.com>
2018-04-16 11:27:02 -0700

    added colors and styles to navbardropdown

--- src/vue/components/NavBar.vue
+++ src/vue/components/NavBar.vue
@@ -1,12 +1,22 @@
 <script>
+  import NavBarDropdown from './NavBarDropdown.vue';
   export default {
     computed: {},
-    components: {},
+    components: { NavBarDropdown },
     data: function () {
-      return {}
+      return {
+        dropdownState: false
+      }
     },
     props: ['listEquipment'],
-    methods: {},
+    methods: {
+       toggleState: function () {
+         this.dropdownState = !this.dropdownState;
+         },
+         test: function (data) {
+           console.log (data)
+         }
+    },
     watch: {}
   }
 </script>
@@ -18,13 +28,23 @@
       <router-link :to="'/' + equipment.name" :key="equipment.name" >{{equipment.name}}</router-link>
     </template>
     <router-link to="/Settings">Settings</router-link>
+    <button @click="toggleState">test</button>
+    <button @click="test">test</button>
   </div>
-  <div class="navbarSize">
-
-  </div>
+  <div class="navbarSize" />
+  <NavBarDropdown v-if="dropdownState" :list="['Controls', 'Power Logs']" />
   </div>
 </template>
 
 <style>
-
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .2s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to {
+  transform: translateX(-10px);
+  opacity: 0;
+}
 </style>
---
+++ src/vue/components/NavBarDropdown.vue
@@ -0,0 +1,29 @@
+<script>
+  export default {
+    computed: {},
+    components: {},
+    data: function () {
+      return {}
+    },
+    props: ['list'],
+    methods: {},
+    watch: {}
+  }
+</script>
+
+<template>
+<transition name="slide-fade" appear>
+  <div class="navBarDropdown">
+    <ul>
+      <li v-for="name in list"
+        :key="name">
+        {{name}}
+      </li>
+    </ul>
+  </div>
+</transition>
+</template>
+
+<style>
+
+</style>
