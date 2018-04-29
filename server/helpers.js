d1c2d093d08316eddc4292c875a48b6366d6416d
Jeffrey Stocker <kagesennin@gmail.com>
2018-04-28 19:33:41 -0700

    init function to make it easier for for loops

--- server/helpers.js
+++ server/helpers.js
@@ -13,3 +13,54 @@ module.exports.returnValueFromObject = function (object, path) {
   }
   return object;
 };
+
+///// wip
+var equalComplex = function (val) {
+  return this.varsToTest.reduce(test => {
+    if (typeof test === 'function') {
+      if (test.call(null, val) === true) {
+        return true;
+      }
+    } else {
+      if (test === val) {
+        return true;
+      }
+    }
+  }, false);
+};
+
+
+var equalOr = function (val) {
+  var equalValues = [...arguments];
+  return this.equalValues.some(currentTest => val === currentTest);
+};
+
+
+var equal = function (val) {
+  return this.varsToTest.every(currentTest => val === currentTest);
+};
+
+var notEqual = function (val) {
+  return !this.varsToTest.every(currentTest => val === currentTest);
+};
+
+
+var notEqualOr = function (val) {
+  return !this.varsToTest.includes(val);
+};
+
+
+module.exports.or = function() {
+  var varsToTest = [...arguments];
+  return {
+    equal,
+    notEqual,
+    notEqualOr,
+    equalOr
+  };
+};
+
+module.exports.and = function () {
+
+};
+///// end WIP
