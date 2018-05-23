23d9ca3a9320adf9d284e5b795af0290284a9fbb
Jeffrey Stocker <kagesennin@gmail.com>
2018-05-23 00:19:27 -0700

    init pentairQueue tests

---
+++ spec/test_pentairQueue.js
@@ -0,0 +1,16 @@
+const chai = require('chai');
+const expect = chai.expect;
+const fs = require('fs');
+const sinon = require ('sinon');
+const moment = require('moment');
+const path = require('path');
+
+// const pentairQueue = require (path.resolve (__dirname + '/../server/equipment/pentair/PentairQueue'));
+
+describe ('pentairQueue', function () {
+  describe ('', function () {
+    it('should ', function () {
+
+    });
+  });
+});
