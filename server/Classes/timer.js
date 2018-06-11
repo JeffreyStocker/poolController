cf51d09ecb28eeafad7c7098c0c21dd7221f525e
Jeffrey Stocker <kagesennin@gmail.com>
2018-06-10 17:49:45 -0700

    Feat: added ability to change duration and function of timer

--- server/Classes/timer.js
+++ server/Classes/timer.js
@@ -1,6 +1,11 @@
 var mixin = {
   start() {
-    this._timer = setInterval(this._callback, this._duration);
+    if (!this._timer) {
+      this._timer = setInterval(() => {
+        this._callback();
+      }, this._duration);
+    }
+    return this._timer;
   },
   stop() {
     clearInterval(this._timer);
@@ -9,18 +14,30 @@ var mixin = {
   reset() {
     this.stop();
     this.start();
-  }
+  },
+  setDuration (time) {
+    if (typeof time !== 'number') { throw new Error ('time:', time, 'should be a number'); }
+    if (typeof time > 0) { throw new Error ('time:', time, 'should be postive'); }
+    this._duration = time;
+    this.reset();
+  },
+  setFunction (newFunc) {
+    if (typeof newFunc !== 'function') { throw new Error ('should be a function'); }
+    this._callback = newFunc;
+    this.reset();
+  },
 };
 
 module.exports = function Timer (duration, callback, autoStart = true) {
   var myVar = Object.create(mixin);
-  if (typeof duration !== 'number' && typeof callback !== 'function') {
-    throw new Error('Duration must be a number, and callback must be a function');
-  }
-  myVar._timer = autoStart ? setInterval(callback, duration) : null;
+
+  typeof duration !== 'object' && new Error('Duration must be a number');
+  typeof callback !== 'function' && new Error('Callback must be a function');
+
   myVar._duration = duration;
   myVar._callback = callback;
+  myVar._context = this;
+  myVar._timer = autoStart ? myVar.start() : null;
 
   return myVar;
 };
-// Object.assign(module.exports.prototype, mixin);
