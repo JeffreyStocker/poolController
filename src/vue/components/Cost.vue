4864b10c7a0dcc27c134e51cbe86f3e516a68b7a
Jeffrey Stocker <kagesennin@gmail.com>
2018-06-29 17:29:55 -0700

    implemented cost, displaypower

---
+++ src/vue/components/Cost.vue
@@ -0,0 +1,28 @@
+<script>
+  export default {
+    computed: {
+      kilowatt () {
+        return this.watts / 1000;
+      },
+      cost () {
+        return (~~(this.kilowatt * 0.33 * 100)) / 100;
+      }
+    },
+    components: {},
+    data: function () {
+      return {}
+    },
+    props: ['watts'],
+    methods: {},
+    watch: {}
+  }
+</script>
+
+<template>
+  <div>
+    This cost you: ${{ this.cost }}
+  </div>
+</template>
+
+<style>
+</style>
---
+++ src/vue/components/DisplayPower.vue
@@ -0,0 +1,42 @@
+<script>
+  var prefix = ['', 'kilo', 'mega', 'giga', 'tera', 'peta', 'exa', 'zetta', 'yotta'];
+
+  export default {
+    computed: {},
+    components: {},
+    data: function () {
+      return {
+        powerUnits: 'watts',
+        shrunkPower: 0
+      }
+    },
+    props: ['watts'],
+    methods: {
+      convertToLargestWattUnits(watts) {
+        var count = 0;
+        var base = 'watts'
+        while (watts > 10000) {
+          count++;
+          watts = watts / 1000;
+        }
+        return [prefix[count] + base, watts];
+      }
+    },
+    watch: {
+      watts () {
+        var shrunkPower =
+        [this.powerUnits, this.shrunkPower] = this.convertToLargestWattUnits(this.watts);
+      }
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <!-- <div>Power: {{this.totalPower}}</div> -->
+    <div>Current View Power: {{this.shrunkPower}} {{this.powerUnits}}</div>
+  </div>
+</template>
+
+<style>
+</style>
