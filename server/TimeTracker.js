312370911d8feab7e5d000a9a419bfe40567af8c
Jeffrey Stocker <kagesennin@gmail.com>
2018-06-29 17:25:16 -0700

    seperated function to keep code drier

--- server/TimeTracker.js
+++ server/TimeTracker.js
@@ -1,16 +1,19 @@
+var _defaultArray = function (time, message) {
+  return [time, message];
+};
 module.exports = class TimeTracker {
   constructor(message) {
     this.start = Date.now();
-    this.times = [[this.start, message]];
+    this.times = [_defaultArray(this.start, message)];
   }
 
   mark(message) {
-    this.times.push([Date.now(), message]);
+    this.times.push(Date.now(), _defaultArray(message));
   }
 
   end(message) {
     this.end = Date.now();
-    this.times.push([this.end, message]);
+    this.times.push(this.end, _defaultArray(message));
     console.log('Total Time Difference:', this.end - this.start);
   }
 };
