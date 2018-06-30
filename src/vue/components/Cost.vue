28df7d93338477dbe805245b523f1ca51ac5e6d8
Jeffrey Stocker <kagesennin@gmail.com>
2018-06-29 19:41:50 -0700

    feat: cost.vue show cents with always 2 digits

--- src/vue/components/Cost.vue
+++ src/vue/components/Cost.vue
@@ -5,7 +5,17 @@
         return this.watts / 1000;
       },
       cost () {
-        return (~~(this.kilowatt * 0.33 * 100)) / 100;
+        var cost = (~~(this.kilowatt * 0.33 * 100)) / 100;
+        var costStr = ('' + cost).split('.')
+        if (costStr[1] !== undefined) {
+
+          while(costStr[1].length < 2) {
+            costStr[1] += '0';
+          }
+        } else {
+          costStr[1] = '00'
+        }
+        return costStr.join('.');
       }
     },
     components: {},
