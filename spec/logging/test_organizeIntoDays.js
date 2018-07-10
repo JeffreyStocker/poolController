60d18693a8a4a80884db17fc8fe2b3deb406208f
Jeffrey Stocker <kagesennin@gmail.com>
2018-07-10 09:26:06 -0700

    init: test organizeIntoDays

---
+++ spec/logging/test_organizeIntoDays.js
@@ -0,0 +1,34 @@
+const rewire = require('rewire');
+const organizeIntoDay = rewire (__dirname + '/../../server/logging/organizeIntoDays.js');
+const currentLogs = rewire(__dirname + '/../../server/logging/CurrentLogs.js');
+const chai = require('chai');
+const expect = chai.expect;
+const PouchDB = require('pouchdb');
+PouchDB.plugin(require('pouchdb-find'));
+
+// const fs = require('fs');
+// const sinon = require ('sinon');
+// const moment = require('moment');
+
+// myModule.__set__("path", "/dev/null");
+// myModule.__get__("path"); // = '/dev/null'
+
+describe ('organizeIntoDays', function () {
+  var oldDB = organizeIntoDay.__get__('db');
+  before ( function (done) {
+    oldDB.close()
+      .then (function () {
+        organizeIntoDay.__set__('db', PouchDB('./testCurrentDB'));
+        done();
+      });
+  });
+
+  after (function () {
+    // currentLogs.__with__()
+  });
+  describe ('', function () {
+    it('should ', function () {
+
+    });
+  });
+});
