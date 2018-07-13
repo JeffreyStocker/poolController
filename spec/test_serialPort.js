a27c3cc4fef372277196c03df2e5955504037d52
Jeffrey Stocker <kagesennin@gmail.com>
2018-07-12 22:35:08 -0700

    organized test_serialPort to not run code when other test has priority

--- spec/test_serialPort.js
+++ spec/test_serialPort.js
@@ -5,12 +5,12 @@ const SerialPort = require('serialport/test'); // when installed as a package
 const sp = require(__dirname + '/../server/communications/serialPort.js'); // when installed as a package
 const MockBinding = SerialPort.Binding;
 
-const portPath = 'COM_ANYTHING';
-
-// MockBinding.createPort(portPath, { echo: false, record: false });
-const port = new SerialPort(portPath);
-
-describe ('', function () {
+describe ('SerialPort', function () {
+  const portPath = 'COM_ANYTHING';
+  before (function () {
+    MockBinding.createPort(portPath, { echo: false, record: false });
+    const port = new SerialPort(portPath);
+  });
   describe ('', function () {
     it('should ', function () {
       expect().to.equal();
