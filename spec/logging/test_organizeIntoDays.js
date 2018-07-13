ffc00dd04802fd6fb5bbaf3f3a2ac8ccb5b23c3e
Jeffrey Stocker <kagesennin@gmail.com>
2018-07-12 22:35:52 -0700

    test: replicated database to not touch sample Databse

--- spec/logging/test_organizeIntoDays.js
+++ spec/logging/test_organizeIntoDays.js
@@ -12,21 +12,50 @@ PouchDB.plugin(require('pouchdb-find'));
 
 // myModule.__set__("path", "/dev/null");
 // myModule.__get__("path"); // = '/dev/null'
+var sampleData = PouchDB(__dirname + '/../SampleDatabase/power');
 
-describe ('organizeIntoDays', function () {
-  var oldDB = organizeIntoDay.__get__('db');
+describe.only ('organizeIntoDays', function () {
+  var oldDB, db;
   before ( function (done) {
-    oldDB.close(function (err) {
-      err && console.log (err);
-      organizeIntoDay.__set__('db', PouchDB('./testCurrentDB'));
-      done();
-    });
+    oldDB = organizeIntoDay.__get__('db');
+    db = PouchDB(__dirname + '/testCurrentDB');
+
+    PouchDB.replicate(sampleData, db)
+      .then(results => {
+        // db.info().then(console.log);
+        // console.log('results:', results);
+        organizeIntoDay.__set__('db', db);
+        // oldDB.close(function (err) {
+        //   err && console.log ('close', err);
+        // });
+        done();
+      });
   });
 
   after (function () {
-    // currentLogs.__with__()
+    currentLogs.__with__('db', oldDB);
+    db.destroy();
   });
-  describe ('', function () {
+
+  describe ('updateDatabase', function () {
+    it('should ', function () {
+
+    });
+  });
+
+  describe ('combineIntoHours', function () {
+    it('should ', function () {
+
+    });
+  });
+
+  describe ('deepSum', function () {
+    it('should ', function () {
+
+    });
+  });
+
+  describe ('getAndCombineDataIntoHours', function () {
     it('should ', function () {
 
     });
